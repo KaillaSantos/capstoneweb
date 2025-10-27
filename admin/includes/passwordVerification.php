d804de4617aa37590c614e512a906367e9634577
Kailla Santos <kaillasantos.basc@gmail.com>
2025-10-27 13:06:34 +0800

    changes passwordverfication

--- admin/includes/passwordVerification.php
+++ admin/includes/passwordVerification.php
@@ -27,7 +27,6 @@ if (isset($_POST['verify_submit'])) {
 
     if ($isValid) {
       echo "<script>
-        <script>alert('Incorrect password. Please try again.');</script>;
         window.location.href = '/capstoneweb/admin/pages/accsetting.php?userid={$userid}';
       </script>";
       exit();
