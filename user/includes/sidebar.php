3018353e4f583d9c265931f9a527e57402ca197b
Kailla Santos <kaillasantos.basc@gmail.com>
2025-10-27 17:00:18 +0800

    user sidebar

--- user/includes/sidebar.php
+++ user/includes/sidebar.php
@@ -4,8 +4,6 @@ $result = mysqli_query($conn, $query);
 $user = mysqli_fetch_assoc($result);
 
 $userImage = !empty($user['userimg']) ? "../image/" . $user['userimg'] : "../image/placeholder.jpg";
-
-$current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <div class="sidebar">
@@ -15,20 +13,19 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <div class="profile-info">
       <h3><?= htmlspecialchars($user['userName']) ?></h3>
       <p ><?= htmlspecialchars($user['email']) ?></p>
-      <p> Purok <?= htmlspecialchars($user['purok']) ?></p>
     </div>
   </div>
 
   <div class="sidenav">
-    <a href="/capstoneweb/admin/pages/announcement.php?userid=<?= $userid ?>" class="<?= ($current_page == 'announcement.php') ? 'active' : '' ?>"><i class="fas fa-bell"></i> <span class="link-text"> Announcement</span></a>
-    <a href="/capstoneweb/admin/pages/record.php?userid=<?= $userid ?>" class="<?= ($current_page == 'record.php') ? 'active' : '' ?>"><i class="fas fa-clipboard-list"></i><span class="link-text"> Records</span></a>
-    <a href="/capstoneweb/admin/pages/recyclables.php?userid=<?= $userid ?>" class="<?= ($current_page == 'recyclables.php') ? 'active' : '' ?>"><i class="fas fa-recycle"></i><span class="link-text"> Recyclables</span></a>
-    <a href="/capstoneweb/admin/pages/reward.php?userid=<?= $userid ?>" class="<?= ($current_page == 'reward.php') ? 'active' : '' ?>"><i class="fa-solid fa-award"></i><span class="link-text"> Reward</span></a>
-    <a href="/capstoneweb/admin/pages/notification.php?userid=<?= $userid ?>" class="<?= ($current_page == 'notification.php') ? 'active' : '' ?>"><i class="fas fa-exclamation-circle"></i><span class="link-text"> Notification</span></a>
-    <a href="#" data-bs-toggle="modal" data-bs-target="#verifyPasswordModal" class="<?= ($current_page == 'accsetting.php') ? 'active' : '' ?>">
+    <a href="/capstoneweb/user/pages/user_announcement.php?userid=<?= $userid ?>"><i class="fas fa-bell"></i> <span class="link-text"> Announcement</span></a>
+    <a href="/capstoneweb/user/pages/user_records.php?userid=<?= $userid ?>"><i class="fas fa-clipboard-list"></i><span class="link-text"> Records</span></a>
+    <a href="/capstoneweb/user/pages/user_recyclables.php?userid=<?= $userid ?>"><i class="fas fa-recycle"></i><span class="link-text"> Recyclables</span></a>
+    <a href="/capstoneweb/user/pages/user_rewards.php?userid=<?= $userid ?>"><i class="fa-solid fa-award"></i><span class="link-text"> Reward</span></a>
+    <a href="/capstoneweb/user/pages/user_notification.php?userid=<?= $userid ?>"><i class="fas fa-exclamation-circle"></i><span class="link-text"> Notification</span></a>
+    <a href="#" data-bs-toggle="modal" data-bs-target="#verifyPasswordModal">
       <i class="fa-solid fa-gears"></i> <span class="link-text"> Account Settings </span>
     </a>
-    <a href="/capstoneweb/admin/function/logout.php"><i class="fas fa-door-open"></i><span class="link-text"> Logout </span></a>
+    <a href="/capstoneweb/user/function/logout.php"><i class="fas fa-door-open"></i><span class="link-text"> Logout </span></a>
   </div>
 
   <script>
@@ -57,4 +54,10 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </div>
+
+
+  
+
+ 
+
 </div>
