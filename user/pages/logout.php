637b7bef5894d925e49e58b4d6c0ea7112fdb394
Kailla Santos <kaillasantos.basc@gmail.com>
2025-10-28 14:33:16 +0800

    update user logout

--- user/pages/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require '../conn/dbconn.php';
-
-    session_start();
-    unset($_SESSION['userName']);
-    session_destroy();
-    header("location:\capstoneweb\index.php");
-    exit();
-?>
