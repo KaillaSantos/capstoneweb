b6548472c8dc277a45ec520bbac985824913a366
Kailla Santos <kaillasantos.basc@gmail.com>
2025-10-30 15:17:31 +0800

    correct pathway edit button

--- admin/pages/announcement.php
+++ admin/pages/announcement.php
@@ -125,7 +125,7 @@
                     Read More »
                   </button>
 
-                  <a href="editannouncement.php?id=<?= $rows['announce_id'] ?>"
+                  <a href="../pages/editannouncement.php?<?= $rows['announce_id'] ?>"
                     class="btn btn-warning btn-sm">
                     <i class="fa fa-edit"></i> Edit
                   </a>
