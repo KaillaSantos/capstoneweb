ab0135b3d6ac64db52f9984697af802a3c63acdb
Kailla Santos <kaillasantos.basc@gmail.com>
2025-11-05 21:25:37 +0800

    fix css passwordrecover

--- recoverPassword.php
+++ recoverPassword.php
@@ -134,7 +134,7 @@ try {
   </header>
   <div class="container mt-5" style="padding-top:250px">
     <div class="col-md-5 mx-auto">
-      <div class="card p-4" style="padding:10px">
+      <div class="card" style="padding:30px">
         <h3 class="mb-3" style="align-content:center;">Recover Password</h3>
         <?= $message; ?>
         <form method="post">
